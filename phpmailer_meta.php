7dee9ad72372d6d1ae46138eb6b0fa213013005c
MrCheat <40684650+MrCheatEugene@users.noreply.github.com>
2024-02-14 23:42:44 +0300

    Example package meta-file

---
+++ phpmailer_meta.php
@@ -0,0 +1,9 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require 'deps/phpmailer/phpmailer/PHPMailer-6.9.1/src/Exception.php';
+require 'deps/phpmailer/phpmailer/PHPMailer-6.9.1/src/PHPMailer.php';
+require 'deps/phpmailer/phpmailer/PHPMailer-6.9.1/src/SMTP.php';
+
+?>
